349a9e9899509452d317e018c165cc54b8e998d5
Neeraj <neeraj.nathany@gmail.com>
2022-11-02 23:58:23 +0530

    Visual fidelity commit

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,10 @@
-const types = ['Food','Travel','Hotel'];
+const types = ['Food','Takeaway','Travel','Hotel','Others'];
+const type_thumbs = {
+    'Food':'https://i.ibb.co/SXw0bGY/perspective-matte-317-128x128.png',
+    'Takeaway':'https://i.ibb.co/Jn2YGMz/perspective-matte-327-128x128.png',
+    'Travel':'https://i.ibb.co/BZNk9D2/perspective-matte-68-128x128.png',
+    'Hotel':'https://i.ibb.co/vjP6SJy/Home-perspective-matte-128x128.png',
+    'Others':'https://i.ibb.co/TKLYWLr/perspective-matte-28-128x128.png'
+}
 
-export {types};
+export {types, type_thumbs};
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React, {useState, Component} from 'react';
 import DatePicker from "react-datepicker";
 import axios from 'axios';
-import { render } from 'react-dom';
-import { types } from '../constants';
+import Link from '../components/Link';
+import { types, type_thumbs } from '../constants';
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -16,14 +16,11 @@ class Dashboard extends Component {
     
     getExpenses = async() => {
         const response = await axios.create({baseURL: 'http://localhost:5000/'}).get('/expenses');
-        // const [startDate, setStartDate] = useState(new Date());
         this.setState({expenses: response.data.expenses});
-        //console.log(this.state.expenses);
     }
 
     dateChange = (date) => {
         this.setState({startDate:date})
-        // console.log(this.state.expenses[0].date.toDateString());
     }
 
     entityChange = (e) => {
@@ -33,44 +30,54 @@ class Dashboard extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         const expense = {date: this.state.startDate, entity:this.state.entity};
-        //console.log(expense);
         axios.post('http://localhost:5000/', expense).then(response => console.log(response.data)).catch(error => console.log(error));
         this.getExpenses();
+        this.setState({entity:''});
     }
 
     render(){
         return (
-            <div>
-                <DatePicker selected={this.state.startDate} onChange={(date) => this.dateChange(date)} />
-                <form onSubmit={this.onSubmit}>
-                    <input type="text" name="entity" placeholder="Enter expense entity" autoFocus value={this.state.entity} onChange={this.entityChange}/>
-                    <button type="submit" name="submit" value="regular">Submit</button>
-                </form>
-                <ul>
-                    {/* {this.state.expenses.filter( i => {
-                        return i.type !== null;
-                    }).map( (each,num) => {
-                        return (<li key = {num}>{types[each.type]}</li>)
-                    })} */}
-                    {/* {this.state.expenses.map((e,n) => {
-                            return (<li key={n}>{new Date(e.date).toDateString()}</li>)
-                        })
-                    } */}
-                    {
-                        this.state.expenses.filter(i => {
-                            return new Date(i.date).toDateString() === this.state.startDate.toDateString();
-                        }).map((e,n)=>{
-                            return (<li key={n}>{new Date(e.date).toDateString()}</li>)
-                        })
-                    }
-                    {/* {
-                        this.state.expenses.filter(i => {
-                            return i.date.getDate() === this.state.startDate.getDate();
-                        }).map((e,n)=>{
-                            return (<li key={n}>{e.date}</li>)
-                        })
-                    } */}
-                </ul>
+            <div className="dashboard">
+                <header className="header">
+                    <Link className="header-title" href="/">Contra</Link>
+                    <span className="header-user">Neeraj Nathany</span>
+                </header>
+                <main className="main">
+                    <div className="main-content">
+                        <aside className="section-left">
+                            <DatePicker selected={this.state.startDate} onChange={(date) => this.dateChange(date)} open/>
+                            <form onSubmit={this.onSubmit}>
+                                <input type="text" name="entity" placeholder="Enter expense entity" autoFocus value={this.state.entity} onChange={this.entityChange}/>
+                                <button type="submit" name="submit" value="regular">Submit</button>
+                            </form>
+                        </aside>
+                        <div className="section-right">
+                            <h5 className="section-title"><span>Expenses on </span>{this.state.startDate.toLocaleDateString(undefined,{ weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })}</h5>
+                            <ul>
+                                {
+                                    this.state.expenses.filter(i => {
+                                        return new Date(i.date).toDateString() === this.state.startDate.toDateString();
+                                    }).map((e,n)=>{
+                                        return (
+                                            <li key={n} className="expense-tile">
+                                                <div className="expense-details">
+                                                    <div className="expense-thumb">
+                                                        <img src={type_thumbs[types[e.type+1?e.type:4]]} alt="" />
+                                                    </div>
+                                                    {/* <span className="expense-type">{Boolean(e.type).toString()}</span> */}
+                                                    <div>
+                                                        <span className="expense-entity">{e.entity}</span>
+                                                        {e.amount&&<span className="expense-amount">₹{e.amount}</span>}
+                                                    </div>
+                                                </div>
+                                                <div className={"expense-status "+(e.status)}></div>
+                                            </li>)
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </main>
             </div>
         )
     }
