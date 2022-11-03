bf16a532b1887475f81678463a3794f4019e5bfc
Neeraj <neeraj.nathany@gmail.com>
2022-11-03 16:34:04 +0530

    Select options working

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -8,7 +8,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 class Dashboard extends Component {
 
-    state = {expenses:[], startDate:new Date(), entity: '', pushId:''}
+    state = {expenses:[], startDate:new Date(), entity: '', type:undefined}
 
     componentDidMount(){
         this.getExpenses();
@@ -26,13 +26,29 @@ class Dashboard extends Component {
     entityChange = (e) => {
         this.setState({entity: e.target.value});
     }
+
+    typeChange = (e) => {
+        this.setState({type:e.target.value});
+    }
+
+    statusChange = (identity) => {
+        axios.patch('http://localhost:5000/status/'+identity,{})
+        .then(response => {
+            this.getExpenses();
+        })
+        .catch(error => console.log(error));
+    }
     
     onSubmit = (e) => {
         e.preventDefault();
-        const expense = {date: this.state.startDate, entity:this.state.entity};
-        axios.post('http://localhost:5000/', expense).then(response => console.log(response.data)).catch(error => console.log(error));
+        const expense = {date: this.state.startDate, entity:this.state.entity, type:this.state.type};
+        axios.post('http://localhost:5000/', expense)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => console.log(error));
+        this.setState({entity:''});        
         this.getExpenses();
-        this.setState({entity:''});
     }
 
     render(){
@@ -47,6 +63,14 @@ class Dashboard extends Component {
                         <aside className="section-left">
                             <DatePicker selected={this.state.startDate} onChange={(date) => this.dateChange(date)} open/>
                             <form onSubmit={this.onSubmit}>
+                                <select value={this.state.type} defaultValue='prompt' name="type" onChange={this.typeChange} required>
+                                    <option value='prompt' disabled>Select expense type</option>
+                                    <option value='0'>Eat-out</option>
+                                    <option value='1'>Takeaway</option>
+                                    <option value='2'>Travel</option>
+                                    <option value='3'>Hotel stay</option>
+                                    <option value='4'>Miscellaneous</option>
+                                </select>
                                 <input type="text" name="entity" placeholder="Enter expense entity" autoFocus value={this.state.entity} onChange={this.entityChange}/>
                                 <button type="submit" name="submit" value="regular">Submit</button>
                             </form>
@@ -59,18 +83,20 @@ class Dashboard extends Component {
                                         return new Date(i.date).toDateString() === this.state.startDate.toDateString();
                                     }).map((e,n)=>{
                                         return (
-                                            <li key={n} className="expense-tile">
+                                            <li key={n} className={"expense-tile "+(e.status)}>
                                                 <div className="expense-details">
                                                     <div className="expense-thumb">
                                                         <img src={type_thumbs[types[e.type+1?e.type:4]]} alt="" />
                                                     </div>
-                                                    {/* <span className="expense-type">{Boolean(e.type).toString()}</span> */}
                                                     <div>
                                                         <span className="expense-entity">{e.entity}</span>
                                                         {e.amount&&<span className="expense-amount">₹{e.amount}</span>}
                                                     </div>
                                                 </div>
-                                                <div className={"expense-status "+(e.status)}></div>
+                                                <div className={"expense-status "+(e.status)} onClick={()=> this.statusChange(e._id)}>
+                                                    {/* <img src='https://i.ibb.co/bzgJQTg/accept.png' alt="Done"/> */}
+                                                    <span className='expense-status-action'>{e.status?'Filed':'Filed?'}</span>
+                                                </div>
                                             </li>)
                                     })
                                 }
